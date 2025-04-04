241528204ce81f422651f51863f83ce9a8bba752
ADITYA BHISE <136826156+adityabhise1111@users.noreply.github.com>
2025-04-04 20:18:38 +0000

    new

--- 90_middle/main.js
+++ 90_middle/main.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs')
+const router = express.Router()
+const birds = require('./routes/blog')
+
+// ...
+
+app.use('/blog', birds)
+
+router.use((req,res,next)=>{
+    console.log('router middleware')
+    next()
+
+})
 
 app.use(express.static("public"))
 
 app.use((req,res,next)=>{
     console.log('m1')
+    //res.send('hack') //this will stop the request
     next()//without this page contninue loads
 })
 app.use((req,res,next)=>{
-    console.log('m2')
+    fs.writeFileSync('log.txt', `${Date.now()} ${req.method} ${req.url}\n`, {flag: 'a'})
+    console.log(`m1`)
+    req.aditya = 'aditya'
     next()
 })
 
@@ -17,9 +33,20 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/about', (req, res) => {
-    res.send('Hello about!')
+    res.send('Hello about!'+req.aditya)
   })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+//middle ware is a function that has access to the request object (req),
+//  the response object (res),
+//  and the next middleware function in the application’s request-response cycle.
+//  The next middleware function is commonly denoted by a variable named next.
+// middleware functions can perform the following tasks:
+// - Execute any code.
+// - Make changes to the request and the response objects.
+// - End the request-response cycle.
+// - Call the next middleware function in the stack.
