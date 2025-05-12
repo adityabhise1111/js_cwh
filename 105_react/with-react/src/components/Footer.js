593aef38c527c0ae741059a541b3184dd327fa99
ADITYA BHISE <136826156+adityabhise1111@users.noreply.github.com>
2025-05-12 19:40:51 +0000

    reactwhy

---
+++ 105_react/with-react/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
