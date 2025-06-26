05b113fa1ca2a3c258620a40cdbc1797a2870beb
aditya <adityabhise1111@gmail.com>
2025-06-26 23:31:23 +0530

    events

--- 112_react_event/src/App.jsx
+++ 112_react_event/src/App.jsx
@@ -5,29 +5,48 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [name, setname] = useState("name")
+  const [form, setform] = useState({}) // if its () empty then no components is visible
+  const [btnText, setbtnText] = useState("dont click me")
+  const [btnVisibility, setbtnVisibility] = useState(true)
+  function clickalert() {
+    
+    alert('bye bye!')//how to give alert delay? ans= to delay the alert, you can use setTimeout function
+    
+    setbtnVisibility(false)
+    
+  }
+  function hoveralert() {
+    setTimeout(() => {
+      alert('dont touch me!')
+    }, 1000) // this will delay the alert by 1 second
+    
+    setbtnText("I said dont touch me!")
+  }
+  function handlechange(e) {
+    setform({...form, [e.target.name]: e.target.value})
+    console.log(form)
+    // this will update the form state with the new value of the input field
+    //here is no logic just react syntax to update the state
+    //its a unnnecessary react feature that they have provided to update the state
+    //else we use placeholder to and dont set value in input field
+
+    //if we log form here, it will not show the updated value immediately
+    //because state updates are asynchronous in nature
+    //so log goes first and then state updates its late.
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      {btnVisibility && <div className="button">
+
+        <input type="text" value={form.name? form.name:""} name="name" id="name" onChange={handlechange} />
+        <input type="phone" value={form.phone? form.phone:""} name="phone" id="phone" onChange={handlechange} />
+        
+        <button  onClick={clickalert} onMouseOver={hoveralert}>
+          {btnText}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </div>}
     </>
   )
 }
